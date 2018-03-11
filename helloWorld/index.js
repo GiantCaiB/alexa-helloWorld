70eec02fdf88f16c7894705ce6c06f14a08d49b6
Danny <giantcaib@hotmail.com>
2018-03-11 17:39:41 +1100

    version 0.0002

--- helloWorld/index.js
+++ helloWorld/index.js
@@ -9,6 +9,8 @@ var WELCOME = [
     "Hey, ready to serve.",
     "Hi there, is there anything I can do for you?",
     "Hello, I\'m here to help you.",
+    "Aloha, what can I do for you today?",
+
 ];
 //Different finishings
 var GOODBYE = [
@@ -16,7 +18,9 @@ var GOODBYE = [
     "Ok, see you soon.",
     "Sure thing, see ya.",
     "No worries, bye!",
+    "Of course, goodbye!",
 ];
+
 
 //Set Entry Point
 exports.handler = function (event, context, callback) {
@@ -34,19 +38,52 @@ const handlers = {
         this.attributes['speechOutput'] = randomGreeting;
         this.attributes['repromptSpeech'] = 'I beg your pardon?';
         this.emit(':ask',this.attributes['speechOutput'], this.attributes['repromptSpeech']);
+        //this.emit(':tell',this.attributes['speechOutput']);
     },
-
+    //This intent is a simple calculate
+    'CalculateIntent': function() {
+        //Getting values from the user's input request
+        var operator = this.event.request.intent.slots.Operator.value;
+        var numberOne = parseInt(this.event.request.intent.slots.NumberOne.value);
+        var numberTwo = parseInt(this.event.request.intent.slots.NumberTwo.value);
+        var result = 0;
+        switch (operator) {
+            case 'add':
+                result = numberOne + numberTwo;
+                break;
+            case 'subtract':
+                result = numberOne - numberTwo;
+                break;
+            case 'multiply':
+                result = numberOne * numberTwo;
+                break;
+            case 'divide':
+                if(numberTwo!=0){
+                    result = numberOne / numberTwo; 
+                }
+                else{
+                    result = undefined;
+                }
+                break;
+            default:
+                result = numberOne + numberTwo;
+        }
+        this.emit(':tell', "The result is "+ result);
+        //this.emit(':tell', operator + numberOne +"to" + numberTwo+", well, this is a question.");
+    },
+    
+    //Built-in intents
     'AMAZON.HelpIntent': function () {
 
     },
     'AMAZON.CancelIntent': function () {
-        //choos a random goodbye
+        //choose a random goodbye
         var goodbyeIndex = Math.floor(Math.random() * GOODBYE.length);
         var randomGoodbye = GOODBYE[goodbyeIndex];
         this.emit(':tell', randomGoodbye);
     },
     'AMAZON.StopIntent': function () {
-        //choos a random goodbye
+        //choose a random goodbye
         var goodbyeIndex = Math.floor(Math.random() * GOODBYE.length);
         var randomGoodbye = GOODBYE[goodbyeIndex];
         this.emit(':tell', randomGoodbye);
